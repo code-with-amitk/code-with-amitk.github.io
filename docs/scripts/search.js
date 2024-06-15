5f53e77c2aee2077e9f34201db0ee35cd540a088
findingamit <findingamit@yahoo.com>
2024-06-15 11:45:27 +0530

    site

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('searchForm').addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevent the form from submitting normally
+    console.log('DOM fully loaded and parsed');
 
-        var searchQuery = document.getElementById('searchInput').value.toLowerCase();
+    var searchForm = document.getElementById('searchForm');
+    if (!searchForm) {
+        console.error('Search form not found');
+        return;
+    }
+
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+        console.log('Search form submitted');
+
+        var searchInput = document.getElementById('searchInput');
+        if (!searchInput) {
+            console.error('Search input not found');
+            return;
+        }
+
+        var searchQuery = searchInput.value.toLowerCase();
+        console.log('Search query:', searchQuery);
+
         var content = document.getElementById('content');
+        if (!content) {
+            console.error('Content element not found');
+            return;
+        }
+
         var paragraphs = content.getElementsByTagName('p');
+        console.log('Number of paragraphs:', paragraphs.length);
 
         // Reset previous search results
         for (var i = 0; i < paragraphs.length; i++) {
